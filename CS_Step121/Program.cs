a6dda820fe11b1ff5371ac03e7e43f7e880798bc
Gavin Kimberlin <guviner@gmail.com>
2020-05-15 19:16:47 -0700

    Create a program that will determine if a package can be shipped, and then provide a quote for that qualifying package.

--- CS_Step121/Program.cs
+++ CS_Step121/Program.cs
@@ -10,6 +10,37 @@ namespace CS_Step121
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.\r\nPlease enter the package weight: ");
+            byte weight = Convert.ToByte(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                System.Threading.Thread.Sleep(2500);
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Please enter the package width:");
+            byte width = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            byte height = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            byte length = Convert.ToByte(Console.ReadLine());
+
+            int total = (weight * width * height * length) / 100;
+            
+            if ((width + height + length) > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                System.Threading.Thread.Sleep(2500);
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Your estimated total for shipping this package is: $" + total + ".00\r\nThank you.\r\nPress ENTER to close the program.");
+            }
+            Console.ReadLine();
         }
     }
 }
